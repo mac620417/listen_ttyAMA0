5ed0a26c6720e9a861ddcdfc36eb26d68902f9b3
mac620417 <76483779+mac620417@users.noreply.github.com>
2021-07-09 16:15:38 +0800

    Add files via upload

---
+++ listen_ttyAMA0_V1.0.py
@@ -0,0 +1,28 @@
+import serial
+import time
+
+device = '/dev/ttyAMA0'
+baudrate = 115200
+ser = serial.Serial(device, baudrate, timeout=1)
+f = None
+ticks = time.time()
+
+try:
+    while True:
+        linestr = ser.readlins()
+        total = ticks, linestr
+        print (total)
+
+        if linestr:
+            f = open('home/pi/DEsk/the_com1.txt','wt')
+            f.write(str(total))
+            break
+
+except keyboardInterrupt:
+    print ('keyboardInterrupt:')
+
+finally:
+    if ser is not None:
+        ser.close()
+    if f is not None:
+        f.close()
